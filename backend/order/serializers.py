762b869ca03feaf344a4415a543d438cd7a0bfa6
kbyamin <yaminrupom@gmail.com>
2022-05-22 23:48:34 +0600

    update trackSerializer

--- backend/order/serializers.py
+++ backend/order/serializers.py
@@ -9,14 +9,21 @@ class OrderSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class OrderItemSerializer(serializers.ModelSerializer):
-    order_id = OrderSerializer()
+    #order_id = OrderSerializer()
     product = ProductSerializer() 
     
     class Meta:
         model = OrderItem
-        fields = '__all__'
+        fields = ['product', 'amount']
 
 class OrderTrackSerializer(serializers.ModelSerializer):
+    order_item = serializers.SerializerMethodField()
+
     class Meta:
         model = Order
-        fields = ['name', 'phone', 'address', 'order_status', 'total_price']
+        fields = ['name', 'phone', 'address', 'order_status', 'total_price', 'order_item']
+    
+    def get_order_item(self, obj):
+        orderItems = obj.orderitem_set.all()
+        orderItem_serializer = OrderItemSerializer(orderItems, many=True).data
+        return orderItem_serializer
